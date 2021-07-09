b12885cf471b72ebe51288fd1d37352ba6d7d267
alberto <alberto.jauregui@fujitsu.com>
2021-07-09 20:39:59 +0200

    table changed

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,293 +1,123 @@
-import React, { useState } from "react";
-import {
-  useTable,
-  useSortBy,
-  usePagination,
-  useGlobalFilter,
-  useAsyncDebounce,
-} from "react-table";
+import React from "react";
+import styled, { withTheme } from "styled-components";
+import { useTable, useSortBy } from "react-table";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faAngleDown,
-  faAngleUp,
-  faEllipsisV,
-  faAngleLeft,
-  faAngleDoubleLeft,
-  faAngleRight,
-  faAngleDoubleRight,
-} from "@fortawesome/free-solid-svg-icons";
-/* import Input from "../Inputs/InputText";
-import Button from "../Button"; */
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
 
-/* import InputCheckBox from "../Inputs/InputCheckbox"; */
+const Styles = styled.div`
+  padding: 1rem;
 
-function Table({
-  columns,
-  data,
-  onAddButton,
-  tableName,
-  withSearching,
-  withPagination,
-  onRowClick,
-}) {
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    page,
-    prepareRow,
-    allColumns,
-    canPreviousPage,
-    canNextPage,
-    pageOptions,
-    pageCount,
-    gotoPage,
-    nextPage,
-    previousPage,
-    setPageSize,
-    setGlobalFilter,
-    preGlobalFilteredRows,
-    state: { pageIndex, pageSize, globalFilter },
-  } = useTable(
-    {
-      columns,
-      data,
-      initialState: {
-        pageIndex: 0,
-        pageSize: withPagination ? 10 : 10000,
+  table {
+    border-spacing: 0;
+
+    thead {
+      th {
+        color: ${({ theme }) => theme.palette.grey[1]};
+        border-bottom: ${({ theme }) => `2px solid ${theme.palette.grey[1]}`};
+        padding: 10px 50px;
+      }
+    }
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+          text-align: center;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`;
+
+function Table({ columns, data }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable(
+      {
+        columns,
+        data,
       },
-    },
-    useGlobalFilter,
-    useSortBy,
-    usePagination
-  );
-  const [showColumnsOptions, setShowColumnsOptions] = useState(false);
+      useSortBy
+    );
+  const firstPageRows = rows.slice(0, 20);
 
   return (
     <>
-      {withSearching && (
-        <Search
-          globalFilter={globalFilter}
-          setGlobalFilter={setGlobalFilter}
-          preGlobalFilteredRows={preGlobalFilteredRows}
-        />
-      )}
-
-      <table {...getTableProps()} className="w-full">
-        <THead headerGroups={headerGroups} />
-        <TBody
-          getTableBodyProps={getTableBodyProps}
-          page={page}
-          prepareRow={prepareRow}
-          onRowClick={onRowClick}
-        />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers
+                .filter((el) => el.columns === undefined)
+                .map((column) => {
+                  // @ts-ignore
+                  const color = column.isSorted
+                    ? "text-white font-bold"
+                    : "text-primary-light font-thin";
+                  // @ts-ignore
+                  const icon = column.isSorted ? (
+                    // @ts-ignore
+                    column.isSortedDesc ? (
+                      <FontAwesomeIcon
+                        icon={faAngleDown}
+                        className="text-secondary-main text-md"
+                      />
+                    ) : (
+                      <FontAwesomeIcon
+                        icon={faAngleUp}
+                        className="text-secondary-main text-md"
+                      />
+                    )
+                  ) : (
+                    ""
+                  );
+                  return (
+                    <th
+                      // @ts-ignore
+                      {...column.getHeaderProps(column.getSortByToggleProps())}
+                      className={`text-center py-2 bg-primary-dark text-sm ${color}`}
+                    >
+                      {icon}
+                      {` ${column.render("Header")}`}
+                    </th>
+                  );
+                })}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {firstPageRows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
-
-      <PaginationAndAddButton
-        isShown={withPagination}
-        paginationMethods={{
-          canPreviousPage,
-          canNextPage,
-          pageOptions,
-          pageCount,
-          gotoPage,
-          nextPage,
-          previousPage,
-          setPageSize,
-          pageIndex,
-          pageSize,
-        }}
-        onAddButton={onAddButton}
-      />
+      <br />
+      <div>Showing the first 20 results of {rows.length} rows</div>
     </>
   );
 }
 
-const THead = ({ headerGroups }) => (
-  <thead>
-    {headerGroups.map((headerGroup) => (
-      <tr {...headerGroup.getHeaderGroupProps()}>
-        {headerGroup.headers
-          .filter((el) => el.columns === undefined)
-          .map((column) => {
-            // @ts-ignore
-            const color = column.isSorted
-              ? "text-white font-bold"
-              : "text-primary-light font-thin";
-            // @ts-ignore
-            const icon = column.isSorted ? (
-              // @ts-ignore
-              column.isSortedDesc ? (
-                <FontAwesomeIcon
-                  icon={faAngleDown}
-                  className="text-secondary-main text-md"
-                />
-              ) : (
-                <FontAwesomeIcon
-                  icon={faAngleUp}
-                  className="text-secondary-main text-md"
-                />
-              )
-            ) : (
-              ""
-            );
-            return (
-              <th
-                // @ts-ignore
-                {...column.getHeaderProps(column.getSortByToggleProps())}
-                className={`text-center py-2 bg-primary-dark text-sm ${color}`}
-              >
-                {icon}
-                {` ${column.render("Header")}`}
-              </th>
-            );
-          })}
-      </tr>
-    ))}
-  </thead>
-);
-
-const TBody = ({ getTableBodyProps, page, prepareRow, onRowClick }) => (
-  <tbody {...getTableBodyProps()}>
-    {page.map((row, i) => {
-      prepareRow(row);
-      return (
-        <tr
-          {...row.getRowProps()}
-          onClick={onRowClick ? () => onRowClick(row) : undefined}
-          className={onRowClick ? "cursor-pointer" : ""}
-        >
-          {row.cells.map((cell) => {
-            return (
-              <td
-                {...cell.getCellProps()}
-                className="text-center py-2 border-b border-primary-light text-sm text-grey-500 bg-white"
-              >
-                {cell.render("Cell")}
-              </td>
-            );
-          })}
-        </tr>
-      );
-    })}
-  </tbody>
-);
-
-const PaginationAndAddButton = ({
-  paginationMethods,
-  onAddButton,
-  withPagination,
-  isShown,
-}) => (
-  <div className="flex md:flex-row lg:flex-column  justify-center items-center relative mt-6">
-    {isShown && <Pagination paginationMethods={paginationMethods} />}
-    {onAddButton && (
-      <button
-        onClick={onAddButton}
-        text={"commons.add"}
-        color="secondary"
-        className={"absolute right-0 mt-1"}
-        size="sm"
-      ></button>
-    )}
-  </div>
-);
-
-const Search = ({ globalFilter, setGlobalFilter, preGlobalFilteredRows }) => {
-  // const count = preGlobalFilteredRows.length;
-  const [value, setValue] = React.useState(globalFilter);
-  const onChange = useAsyncDebounce((value) => {
-    setGlobalFilter(value || undefined);
-  }, 800);
+export default withTheme(function ({ data, columns }) {
   return (
-    <div className="md:w-2/4 xs:w-full xl:w-1/4">
-      <input
-        value={value}
-        label="Buscar"
-        onChange={(name, value) => {
-          setValue(value);
-          onChange(value);
-        }}
-      />
-    </div>
+    <Styles>
+      <Table columns={columns} data={data} />
+    </Styles>
   );
-};
-
-const Pagination = ({ paginationMethods }) => {
-  const {
-    canPreviousPage,
-    canNextPage,
-    pageOptions,
-    pageCount,
-    gotoPage,
-    nextPage,
-    previousPage,
-    // setPageSize,
-    pageIndex,
-    // pageSize,
-  } = paginationMethods;
-  return (
-    <div
-      className="flex flex-column justify-between items-center m-auto mt-2"
-      style={{ width: "300px" }}
-    >
-      <PaginationButton onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-        <FontAwesomeIcon icon={faAngleDoubleLeft} />
-      </PaginationButton>
-      <PaginationButton
-        onClick={() => previousPage()}
-        disabled={!canPreviousPage}
-      >
-        <FontAwesomeIcon icon={faAngleLeft} />
-      </PaginationButton>
-      <TextPagination pageIndex={pageIndex} pageOptions={pageOptions} />
-      <PaginationButton onClick={() => nextPage()} disabled={!canNextPage}>
-        <FontAwesomeIcon icon={faAngleRight} />
-      </PaginationButton>
-      <PaginationButton
-        onClick={() => gotoPage(pageCount - 1)}
-        disabled={!canNextPage}
-      >
-        <FontAwesomeIcon icon={faAngleDoubleRight} />
-      </PaginationButton>
-      {/*       <span>
-        Page{' '}
-        <strong>
-          {pageIndex + 1} of {pageOptions.length}
-        </strong>{' '}
-      </span>
-      <select
-        value={pageSize}
-        onChange={(e) => {
-          setPageSize(Number(e.target.value));
-        }}
-      >
-        {[5, 10, 15, 20, 50].map((pageSize) => (
-          <option key={pageSize} value={pageSize}>
-            Show {pageSize}
-          </option>
-        ))}
-      </select> */}
-    </div>
-  );
-};
-
-const TextPagination = ({ pageIndex, pageOptions }) => {
-  return (
-    <span className="text-grey-500">
-      {`Page `}
-      {<span className="font-extrabold">{pageIndex + 1}</span>}
-      {` of ${pageOptions.length}`}
-    </span>
-  );
-};
-
-const PaginationButton = ({ children, ...otherProps }) => (
-  <button
-    className="h-8 w-8 bg-primary-dark text-white rounded-md"
-    {...otherProps}
-  >
-    {children}
-  </button>
-);
-export default Table;
+});
