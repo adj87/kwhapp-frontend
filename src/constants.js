5f9b59605e1e29cb6d6e30b6239e4041a0ee1e42
alberto <alberto.jauregui@fujitsu.com>
2021-07-09 17:44:45 +0200

    add theme for styled components

---
+++ src/constants.js
@@ -0,0 +1,33 @@
+export const theme = {
+  breakpoints: {
+    xs: 300,
+    sm: 500,
+    md: 700,
+    lg: 1021,
+  },
+  palette: {
+    primary: {
+      1: "#004B23",
+      2: "#006400",
+      3: "#007200",
+      4: "#008000",
+      5: "#38B000",
+      6: "#70E000",
+      7: "#9EF01A",
+    },
+    blue: "#1AE3F0",
+    error: "#FF5858",
+    success: "#47ad34",
+    grey: {
+      1: "#2D3748",
+      2: "#ACB4C2",
+    },
+  },
+};
+
+export const device = {
+  deviceXs: `(min-width: ${theme.breakpoints.xs}px)`,
+  deviceSm: `(min-width: ${theme.breakpoints.sm}px)`,
+  deviceMd: `(min-width: ${theme.breakpoints.md}px)`,
+  deviceLg: `(min-width: ${theme.breakpoints.lg}px)`,
+};
