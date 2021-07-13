a563427b2006524ca3c15ab369ebe65c0ea252a7
alberto <alberto.jauregui@fujitsu.com>
2021-07-13 17:39:27 +0200

    add more apis

--- src/api.js
+++ src/api.js
@@ -5,5 +5,15 @@ const importData = ({ base64, format }) =>
   axios.post(`${API_URL}/consumptions/import-data`, { base64, format });
 
 const getConsumptions = () => axios.get(`${API_URL}/consumptions`);
+const createConsumption = (consumption) => axios.post(`${API_URL}/consumptions/${id}`, consumption);
+const deleteConsumption = (id) => axios.delete(`${API_URL}/consumptions/${id}`);
+const editConsumption = (id, consumption) =>
+  axios.put(`${API_URL}/consumptions/${id}`, consumption);
 
-export default { importData, getConsumptions };
+export default {
+  importData,
+  getConsumptions,
+  deleteConsumption,
+  editConsumption,
+  createConsumption,
+};
