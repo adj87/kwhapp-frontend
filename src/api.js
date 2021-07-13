ee4be3137b423a2f77b09a7bb052aa77ae4732a8
alberto <alberto.jauregui@fujitsu.com>
2021-07-13 18:01:31 +0200

    correct the apis

--- src/api.js
+++ src/api.js
@@ -5,10 +5,10 @@ const importData = ({ base64, format }) =>
   axios.post(`${API_URL}/consumptions/import-data`, { base64, format });
 
 const getConsumptions = () => axios.get(`${API_URL}/consumptions`);
-const createConsumption = (consumption) => axios.post(`${API_URL}/consumptions/${id}`, consumption);
+const createConsumption = (consumption) => axios.post(`${API_URL}/consumptions`, consumption);
 const deleteConsumption = (id) => axios.delete(`${API_URL}/consumptions/${id}`);
-const editConsumption = (id, consumption) =>
-  axios.put(`${API_URL}/consumptions/${id}`, consumption);
+const editConsumption = (consumption) =>
+  axios.put(`${API_URL}/consumptions/${consumption._id}`, consumption);
 
 export default {
   importData,
