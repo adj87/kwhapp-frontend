65bf96976fec51daa572416832d71cbccb1113d1
alberto <alberto.jauregui@fujitsu.com>
2021-07-14 19:09:01 +0200

    move colorst to constants

--- src/app/Home/ChartModal.js
+++ src/app/Home/ChartModal.js
@@ -12,6 +12,7 @@ import {
 } from "recharts";
 import { MainContext } from "../../contexts/MainContext";
 import Modal from "../../components/Modal";
+import { listOfColorsForCharts } from "../../constants";
 
 const SelectorWrapper = styled.div``;
 const CustomLabel = styled.label`
@@ -78,7 +79,7 @@ export default function App() {
               <Scatter
                 name={`${dataKey}-${day}`}
                 data={data.filter((el) => el.date === day).sort((a, b) => a.time - b.time)}
-                fill={colors[i + 1]}
+                fill={listOfColorsForCharts[i + 1]}
                 line
                 shape="circle"
               />
@@ -89,56 +90,3 @@ export default function App() {
     )
   );
 }
-
-const colors = [
-  "#FF6633",
-  "#FFB399",
-  "#FF33FF",
-  "#FFFF99",
-  "#00B3E6",
-  "#E6B333",
-  "#3366E6",
-  "#999966",
-  "#99FF99",
-  "#B34D4D",
-  "#80B300",
-  "#809900",
-  "#E6B3B3",
-  "#6680B3",
-  "#66991A",
-  "#FF99E6",
-  "#CCFF1A",
-  "#FF1A66",
-  "#E6331A",
-  "#33FFCC",
-  "#66994D",
-  "#B366CC",
-  "#4D8000",
-  "#B33300",
-  "#CC80CC",
-  "#66664D",
-  "#991AFF",
-  "#E666FF",
-  "#4DB3FF",
-  "#1AB399",
-  "#E666B3",
-  "#33991A",
-  "#CC9999",
-  "#B3B31A",
-  "#00E680",
-  "#4D8066",
-  "#809980",
-  "#E6FF80",
-  "#1AFF33",
-  "#999933",
-  "#FF3380",
-  "#CCCC00",
-  "#66E64D",
-  "#4D80CC",
-  "#9900B3",
-  "#E64D66",
-  "#4DB380",
-  "#FF4D4D",
-  "#99E6E6",
-  "#6666FF",
-];
